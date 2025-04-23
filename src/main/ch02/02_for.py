40d7876bce5abc44cf899b2f6144b0511fbfa1b5
kangsulim <rkdtnfla1@naver.com>
2025-04-23 15:27:34 +0900

    asd

--- src/main/ch02/02_for.py
+++ src/main/ch02/02_for.py
@@ -70,4 +70,46 @@ for student in students:
 
 print(result)
 
+# 점프 투 파이썬 for문 중
+# 컴프리핸션
+
+a = [1,2,3,4]
+result = [num * 3 for num in a]
+print(result)
+
+
+result = [num * 3 for num in a if num % 2 == 0]
+print(result)
+
+# 이중 for문도 가능
+
+print()
+
+for x in range(2, 10):
+    for y in range(1, 10):
+        if y == 1:
+            print(f"{x}단", end="\t\t")
+        print(f"{x} X {y} = {x*y}", end="\t") if (y != 9) else print(f"{x} X {y} = {x*y}")
+# 3항 연산자 형태
+# a if 조건 else b
+
+
+print()
+
+
+print("2단\t\t\t3단\t\t\t4단\t\t\t5단\t\t\t6단\t\t\t7단\t\t\t8단\t\t\t9단")
+for x in range(1, 10):
+    for y in range(2, 10):
+        multiple = x*y
+        print(f"{y} X {x} = {multiple}", end="\t")
+    print()
+
+
+
+
+
+
+
+
+
 
